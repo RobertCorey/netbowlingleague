5e187b494ccd758946a12a90b353e85c39ca2956
Rob Corey <robert.corey@kensho.com>
2023-12-07 13:54:07 -0500

    update header

--- components/testimonials.js
+++ components/testimonials.js
@@ -6,7 +6,7 @@ import userOneImg from "../public/img/user1.jpg";
 import userTwoImg from "../public/img/user2.jpg";
 import userThreeImg from "../public/img/user3.jpg";
 
-const Testimonials  = () => {
+const Testimonials = () => {
   return (
     <Container>
       <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
@@ -17,11 +17,7 @@ const Testimonials  = () => {
               that hits some of your benefits from one of your popular customer.
             </p>
 
-            <Avatar
-              image={userOneImg}
-              name="Sarah Steiner"
-              title="VP Sales at Google"
-            />
+            <Avatar image={userOneImg} name="Sarah Steiner" title="VP Sales at Google" />
           </div>
         </div>
         <div className="">
@@ -31,43 +27,28 @@ const Testimonials  = () => {
               to keep it short and simple.
             </p>
 
-            <Avatar
-              image={userTwoImg}
-              name="Dylan Ambrose"
-              title="Lead marketer at Netflix"
-            />
+            <Avatar image={userTwoImg} name="Dylan Ambrose" title="Lead marketer at Netflix" />
           </div>
         </div>
         <div className="">
           <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
             <p className="text-2xl leading-normal ">
-              This is an <Mark>awesome</Mark> landing page template I&apos;ve seen. I
-              would use this for anything.
+              This is an <Mark>awesome</Mark> landing page template I&apos;ve seen. I would use this for anything.
             </p>
 
-            <Avatar
-              image={userThreeImg}
-              name="Gabrielle Winn"
-              title="Co-founder of Acme Inc"
-            />
+            <Avatar image={userThreeImg} name="Gabrielle Winn" title="Co-founder of Acme Inc" />
           </div>
         </div>
       </div>
     </Container>
   );
-}
+};
 
 function Avatar(props) {
   return (
     <div className="flex items-center mt-8 space-x-3">
       <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
-        <Image
-          src={props.image}
-          width="40"
-          height="40"
-          alt="Avatar"
-          placeholder="blur"
-        />
+        <Image src={props.image} width="40" height="40" alt="Avatar" placeholder="blur" />
       </div>
       <div>
         <div className="text-lg font-medium">{props.name}</div>
@@ -81,7 +62,7 @@ function Mark(props) {
   return (
     <>
       {" "}
-      <mark className="text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">
+      <mark className="text-teal-800 bg-teal-100 rounded-md ring-teal-100 ring-4 dark:ring-teal-900 dark:bg-teal-900 dark:text-teal-200">
         {props.children}
       </mark>{" "}
     </>
