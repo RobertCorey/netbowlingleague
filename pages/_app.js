af49fe4f626da8b3ebd2e830c4f2630e6724b787
Robert Corey <robert.corey@kensho.com>
2024-01-26 10:25:53 -0500

    Add Analytics component import

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
